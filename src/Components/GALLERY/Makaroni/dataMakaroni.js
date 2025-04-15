c5d2ad46445edbbcbf1e21a156c41a2504656eeb
Ryuivan <ryu.ivan@student.umn.ac.id>
2025-04-15 14:43:59 +0700

    fix: video bug

--- src/Components/GALLERY/Makaroni/dataMakaroni.js
+++ src/Components/GALLERY/Makaroni/dataMakaroni.js
@@ -1,6 +1,6 @@
 // import v1 from "../../../assets/gallery/makaroni/makaroni-vid (1).mp4";
-import v2 from "../../../assets/gallery/makaroni/makaroni-vid (2).mp4";
-import v3 from "../../../assets/gallery/makaroni/makaroni-vid (3).mp4";
+// import v2 from "../../../assets/gallery/makaroni/makaroni-vid (2).mp4";
+// import v3 from "../../../assets/gallery/makaroni/makaroni-vid (3).mp4";
 
 const dataMakaroni = [
   {
@@ -11,10 +11,10 @@ const dataMakaroni = [
     type: "img",
     src: "https://cdn.discordapp.com/attachments/1079650552830513222/1123171161497415711/makaroni_8.webp",
   },
-  {
-    type: "vid",
-    src: v3,
-  },
+  // {
+  //   type: "vid",
+  //   src: v3,
+  // },
   {
     type: "img",
     src: "https://cdn.discordapp.com/attachments/1079650552830513222/1123171160331386940/makaroni_5.webp",
@@ -23,10 +23,10 @@ const dataMakaroni = [
     type: "img",
     src: "https://cdn.discordapp.com/attachments/1079650552830513222/1123171160696303697/makaroni_6.webp",
   },
-  {
-    type: "vid",
-    src: v2,
-  },
+  // {
+  //   type: "vid",
+  //   src: v2,
+  // },
   {
     type: "img",
     src: "https://cdn.discordapp.com/attachments/1079650552830513222/1123171158288777257/makaroni_1.webp",
